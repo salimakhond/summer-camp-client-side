88f1fe7c77fdb57274bca33cc3b005322efd4bfa
salimakhond <salimakhond@gmai.com>
2023-06-07 03:42:53 +0600

    login page added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,60 @@
+import { useForm } from "react-hook-form";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(watch("example"));
+
+    return (
+        <div className="px-5 py-[50px] md:py-[80px] lg:py-[130px] bg-white">
+            <div className="card m-auto w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h2 className="text-5xl font-bold text-center">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" {...register("email", { required: true })} name='email' placeholder="Email" className="input input-bordered" />
+                            {errors.email && <span className="text-error mt-2">Email is required</span>}
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" {...register("password", {
+                                required: true,
+                                minLength: 6,
+                                pattern: /^(?=.*[A-Z])(?=.*[!@#$&*]).*$/,
+                            })} name='password' placeholder="Password" className="input input-bordered" />
+                            {errors.password?.type === 'required' && <p className="text-error mt-2">Password is required</p>}
+                            {errors.password?.type === 'minLength' && <p className="text-error mt-2">Your password less than 6 characters</p>}
+                            {errors.password?.type === 'pattern' && <p className="text-error mt-2">Your password must have one capital letter and one special character.</p>}
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Sign In" />
+                        </div>
+                    </form>
+                    <p className='my-5 text-center'>New to Toy Marketplace?
+                        <Link className='text-orange-500 font-bold ml-3' to='/register'>Register</Link>
+                    </p>
+                    <div className="divider">OR</div>
+                    <div className="text-center">
+                        <button className=" btn btn-circle btn-outline btn-error hover:text-white">
+                            <FaGoogle></FaGoogle>
+                        </button>
+                    </div>
+                    <p className="text-success text-center">success</p>
+                    <p className="text-error text-center">error</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
